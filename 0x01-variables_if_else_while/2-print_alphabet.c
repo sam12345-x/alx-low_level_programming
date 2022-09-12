98a5ff5369ff15fe99ee27ab882869f347496d86
Samrawit Tadesse <111275813+sam12345-x@users.noreply.github.com>
2022-09-12 20:58:40 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(void)
 {
-    char ch = '`';
-    for (ch = '`'; ch <= 'z'; ch++)
+    char ch = ' ';
+    for (ch = ' '; ch <= 'z'; ch++)
         putchar(ch);
     return (0);
 }
